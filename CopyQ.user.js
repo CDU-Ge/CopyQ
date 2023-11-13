ea4abfd3dbf910a576cb2464a7a29a8cf7dd508b
VincentGe <cosplox@outlook.com>
2023-11-13 20:12:14 +0800

    v0.0.0.2

--- CopyQ.user.js
+++ CopyQ.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CDU-CopyQ
 // @namespace    CopyQ.CDU
-// @version      0.0.0.1
+// @version      0.0.0.2
 // @description  CopyQ
 // @author       Ge-CDU
 // @match        *://*/*
