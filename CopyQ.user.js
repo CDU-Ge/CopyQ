81db3c376d7bc2d7f6230415f8bc100d02690f1c
VincentGe <cosplox@outlook.com>
2023-11-13 20:10:30 +0800

    base code

--- CopyQ.user.js
+++ CopyQ.user.js
@@ -4,7 +4,7 @@
 // @version      0.0.0.1
 // @description  CopyQ
 // @author       Ge-CDU
-// @match        http://*/*
+// @match        *://*/*
 // @downloadURL  https://github.com/CDU-Ge/CopyQ/raw/main/CopyQ.user.js
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
@@ -15,7 +15,7 @@
         return;
     
     // add button on page side
-    var button = document.createElement('button');
+    const button = document.createElement('button');
     button.innerHTML = 'CopyQ';
     button.style.position = 'fixed';
     button.style.top = '50%';
@@ -23,12 +23,71 @@
     button.style.zIndex = '9999';
     document.body.appendChild(button);
 
-    button.addEventListener('click', function() {
-        var text = window.getSelection().toString();
-        if (text.length > 0) {
-            var clipboard = Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper);
-            clipboard.copyString(text);
+    /**
+     * Extract the main content of the current page
+     */
+    function extractContent(selector) {
+        // Obtain the main text content under an HTML node and delete CSS, JavaScript, etc
+        function extractText(node) {
+            var text = '';
+            if (node.nodeType === Node.TEXT_NODE) {
+                return node.textContent;
+            } else if (node.nodeType === Node.ELEMENT_NODE) {
+                var children = node.childNodes;
+                for (var i = 0; i < children.length; i++) {
+                    text += extractText(children[i]);
+                }
+            }
+            return text;
         }
+        // Remove the CSS, JavaScript, etc
+        function removeScripts(node) {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                if (node.tagName === 'SCRIPT' || node.tagName === 'STYLE' || node.tagName === 'LINK') {
+                    node.remove();
+                } else {
+                    var children = node.childNodes;
+                    for (var i = 0; i < children.length; i++) {
+                        removeScripts(children[i]);
+                    }
+                }
+            }
+        }
+        // Remove the HTML tags
+        function removeTags(node) {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                if (node.tagName !== 'BODY') {
+                    var children = node.childNodes;
+                    for (var i = 0; i < children.length; i++) {
+                        removeTags(children[i]);
+                    }
+                }
+            } else if (node.nodeType === Node.TEXT_NODE) {
+                node.textContent = node.textContent.replace(/\n/g, ' ');
+            }
+        }
+        var node = document.querySelector(selector);
+        removeScripts(node);
+        removeTags(node);
+        return extractText(node);
+    }
+
+    button.addEventListener('click', function() {
+        var text = extractContent('body');
+        console.log('Selected text:', text);
+        if (text.length > 0) {
+            navigator.clipboard.writeText(text).then(function() {
+                console.log('Text copied to clipboard');
+            }, function() {
+                console.error('Failed to copy text to clipboard');
+            });
+        }
+        // log clipboard content
+        navigator.clipboard.readText().then(function(clipboardText) {
+            console.log('Clipboard content:', clipboardText);
+        }, function() {
+            console.error('Failed to read clipboard content');
+        });
     });
    
 })();
